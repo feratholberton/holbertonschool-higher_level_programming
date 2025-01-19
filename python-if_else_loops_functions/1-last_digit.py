47aeb2984db220b6eb19ca090604c1bca2ff8945
Fer <fernando.falcon@holbertonstudents.com>
2025-01-19 10:02:28 -0300

    check number sign

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(abs(number) % 10)
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit = -last_digit
 
 if last_digit > 5:
     compare_result = 'greater than 5'
