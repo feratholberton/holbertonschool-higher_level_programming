a5e06898d41730c929e2dc7f3cfc49663970ce36
Fer <fernando.falcon@holbertonstudents.com>
2025-01-22 14:48:23 -0300

    Test my list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,6 +4,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
     else:
-        print("Element at index {} is {}".format(idx, my_list[idx]))
-
-element_at(my_list, idx)
+        return my_list[idx]
