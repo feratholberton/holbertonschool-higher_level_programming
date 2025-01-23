94939109791c94a83eb1b7d2e74f1578bb306cb9
Fer <fernando.falcon@holbertonstudents.com>
2025-01-23 12:07:42 -0300

    add tuples

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in range(len(row)):
             if element == len(row) - 1:
-                print("{:d}".format(row[element]), end='')
+                print('{:d}'.format(row[element]), end='')
             else:
-                print("{:d}".format(row[element]), end=' ')
+                print('{:d}'.format(row[element]), end=' ')
         print()
---
+++ python-data_structures/7-add_tuple.py.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    first = tuple_a[0] + tuple_b[0]
+    second = tuple_a[1] + tuple_b[1]
+
+    result = (first, second)
+    print(result)
