cbb96d8cdc49ebc843c307861263b8f0b9ccf031
Fer <fernando.falcon@holbertonstudents.com>
2025-01-26 11:52:51 -0300

    Print list taking care of exceptions

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            count += 1
+
+    except IndexError:
+        print('Error: x is greater than list length.')
+        pass
+
+    finally:
+        print('Execution completed, We are exiting...')
+
+    return count
