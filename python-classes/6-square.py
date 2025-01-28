53781cee63f2e52d373fd5b672443bbb3d479dba
Fer <fernando.falcon@holbertonstudents.com>
2025-01-28 11:25:27 -0300

    get & set coordinates

---
+++ python-classes/6-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+''' Define a Square class '''
+
+
+class Square:
+    ''' Square class '''
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self, value):
+        if ():
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print('')
+            return
+        
+        for row in range(self.__position[1]):
+            print('')
+
+        for row in range(self.__size):
+            print(' ' * self.__position[0] + '#' * self.__size)
