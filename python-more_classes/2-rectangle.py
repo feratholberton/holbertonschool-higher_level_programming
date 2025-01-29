70cb5d598ec87252351fc828e3ae3682e9bf07b3
Fer <fernando.falcon@holbertonstudents.com>
2025-01-29 17:22:55 -0300

    Check for 0

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -37,4 +37,6 @@ class Rectangle:
         return self.__width * self.__height
     
     def perimeter(self):
-        return (self.__width * 2) + (self.__height * 2)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
