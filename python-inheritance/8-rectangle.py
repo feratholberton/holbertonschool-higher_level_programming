9c2269091b0b71988e1f0cd81e217f507c505236
Fer <fernando.falcon@holbertonstudents.com>
2025-02-08 19:43:22 -0300

    Import geo

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,17 +2,7 @@
 ''' Define a class '''
 
 
-class BaseGeometry:
-    ''' Empty Base geometry '''
-    def area(self):
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f'{name} must be an integer')
-        if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     ''' Rectangle class '''
