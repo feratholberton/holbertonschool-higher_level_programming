c11e70c977091cbf3bfcdf96a24458bf2d592531
Fer <fernando.falcon@holbertonstudents.com>
2025-02-11 10:56:40 -0300

    spaces removed

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-print(''.join('{:c}'.format(i - (32 * (i % 2))) for i in range(122, 96, -1)), end='')
+print(''.join('{:c}'.format(i-(32*(i%2))) for i in range(122, 96, -1)), end='')
