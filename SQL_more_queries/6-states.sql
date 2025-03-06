99a2e6b4bdbba859c1a1b6ca8e8316723d6d8690
Fer <fernando.falcon@holbertonstudents.com>
2025-03-06 15:53:40 -0300

    Create database with primary key

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- Create table if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use newly created database
+USE hbtn_0d_usa;
+
+-- Create table if not exists
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
