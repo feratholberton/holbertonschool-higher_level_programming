0864ed6d91de548766b66dd31b66542cd36d5eaf
Fer <fernando.falcon@holbertonstudents.com>
2025-03-06 14:22:49 -0300

    Comment

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,4 +4,5 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
+--
 FLUSH PRIVILEGES;
