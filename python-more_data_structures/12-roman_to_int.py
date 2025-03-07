cc46359602078f3b1865b6342a063cbefd072338
Fer <fernando.falcon@holbertonstudents.com>
2025-03-07 08:30:18 -0300

    Code style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,12 +7,12 @@ def roman_to_int(roman_string):
 
     # Roman values
     roman_values = {
-        'I': 1, 
-        'V': 5, 
-        'X': 10, 
+        'I': 1,
+        'V': 5,
+        'X': 10,
         'L': 50,
-        'C': 100, 
-        'D': 500, 
+        'C': 100,
+        'D': 500,
         'M': 1000
     }
 
