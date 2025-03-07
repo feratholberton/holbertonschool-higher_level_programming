c2bc3555bf2541bcd665eb857c7cb728176d11dc
Fer <fernando.falcon@holbertonstudents.com>
2025-03-07 08:02:51 -0300

    group by

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name AS genre, COUNT (*) AS number_of_shows
 FROM tv_genres, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
