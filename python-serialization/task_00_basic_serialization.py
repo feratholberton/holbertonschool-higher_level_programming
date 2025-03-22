09371289a796710e2d62debb6fbb90a07ac8e738
Fer <fernando.falcon@holbertonstudents.com>
2025-03-22 08:42:12 -0300

    dinamic file name

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,9 +1,9 @@
 import json
 
 def serialize_and_save_to_file(data, filename):
-	with open('data.json', 'w') as filename:
-		json.dump(data, filename)
+	with open(filename, 'w') as json_file:
+		json.dump(data, json_file)
 
 def load_and_deserialize(filename):
-	with open('data.json', 'r') as filename:
-		return json.load(filename)
+	with open(filename, 'r') as json_file:
+		return json.load(json_file)
