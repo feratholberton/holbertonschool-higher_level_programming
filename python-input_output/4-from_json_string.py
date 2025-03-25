bd6e8f17bd8a34a0e8cb6daa622024cb16f0cffc
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 00:52:12 -0300

    4. From JSON string to Object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    This is a placeholder template for function docs
+    '''
+    return json.loads(my_str)
