407905ad6548305309d370f684c0c1245ae7dd0b
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 00:19:09 -0300

    Spaces

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,9 +3,10 @@
 This is a placeholder template for module docs
 '''
 
+
 def read_file(filename=''):
-	'''
-	This is a placeholder template for function docs
-	'''
-	with open(filename, 'r', encoding='utf-8') as file:
-		print(file.read(), end='')
+    '''
+    This is a placeholder template for function docs
+    '''
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end='')
