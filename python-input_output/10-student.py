2b0cebd97588e7c9995933eebb26a4baf9a80c4d
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 09:00:42 -0300

    trailing whitespace

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -24,7 +24,7 @@ class Student:
         if isinstance(attrs, list):
             return {
                 key: getattr(self, key)
-                for key in attrs 
+                for key in attrs
                 if isinstance(key, str) and hasattr(self, key)
             }
         return self.__dict__
