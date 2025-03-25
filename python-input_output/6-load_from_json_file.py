5c352b26e3d2986ab2e5fea72e7364bb24a81029
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 01:07:37 -0300

    6. Create object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    This is a placeholder template for function docs
+    '''
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
