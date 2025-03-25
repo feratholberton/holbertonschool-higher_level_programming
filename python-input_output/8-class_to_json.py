bba23cd640ff7402278258ee308b3813107a43ef
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 08:32:08 -0300

    8. Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+
+
+def class_to_json(obj):
+    '''
+    This is a placeholder template for function docs
+    '''
+    return obj.__dict__
