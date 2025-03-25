b3ddfcc440ffbedd7fbe292ced18729884b3aa9b
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 00:16:04 -0300

    Correction of "0. Read file"

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,11 +3,9 @@
 This is a placeholder template for module docs
 '''
 
-def read_file(filename=""):
+def read_file(filename=''):
 	'''
 	This is a placeholder template for function docs
 	'''
-	with open(filename, 'r') as file:
-		content = file.read()
-
-	print(content)
+	with open(filename, 'r', encoding='utf-8') as file:
+		print(file.read(), end='')
