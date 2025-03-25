9526d0e7d795d18ee40611967746794460f7a010
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 00:09:35 -0300

    0. Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+
+def read_file(filename=""):
+	'''
+	This is a placeholder template for function docs
+	'''
+	with open(filename, 'r') as file:
+		content = file.read()
+
+	print(content)
