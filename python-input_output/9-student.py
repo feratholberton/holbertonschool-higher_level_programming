fca5d68e8133cf91b8e04be1e58815b7c689a278
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 08:41:28 -0300

    9. Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        '''
+        This is a placeholder template for function docs
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def class_to_json(self):
+        '''
+        This is a placeholder template for function docs
+        '''
+        return self.__dict__
