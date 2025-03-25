611bd24aeabbd20638bcdd2a57f1077083ec2447
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 00:29:32 -0300

    1. Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    This is a placeholder template for function docs
+    '''
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
