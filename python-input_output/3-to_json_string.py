15d96b153a43232ad7eb947c78f5c9524f574e88
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 00:45:43 -0300

    3. To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    This is a placeholder template for function docs
+    '''
+    return json.dumps(my_obj)
