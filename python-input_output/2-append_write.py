d7e714cbb4faa49cb17a98060aa02d5ea32a2cb2
Fer <fernando.falcon@holbertonstudents.com>
2025-03-25 00:36:03 -0300

    2. Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+This is a placeholder template for module docs
+'''
+
+
+def append_write(filename="", text=""):
+    '''
+    This is a placeholder template for function docs
+    '''
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
