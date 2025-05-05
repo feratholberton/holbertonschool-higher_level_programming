d1a2e4377638e0d8421741989df3d538d6dc7c55
Fer <fernando.falcon@holbertonstudents.com>
2025-05-05 18:27:27 -0300

    1. The last digit | Shorter var name

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,17 +2,16 @@
 import random
 
 number = random.randint(-10000, 10000)
-number_last_digit = abs(number) % 10
+last_digit = abs(number) % 10
 
 if number < 0:
-    number_last_digit = -number_last_digit
+    last_digit = -last_digit
 
-if number_last_digit > 5:
+if last_digit > 5:
     compared_result = 'greater than 5'
-elif number_last_digit == 0:
+elif last_digit == 0:
     compared_result = '0'
 else:
     compared_result = 'less than 6 and not 0'
 
-print(f'Last digit of {number} is {number_last_digit} \
-    and is {compared_result}')
+print(f'Last digit of {number} is {last_digit} and is {compared_result}')
