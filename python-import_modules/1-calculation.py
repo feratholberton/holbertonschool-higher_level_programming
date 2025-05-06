69ee208128651f60867dcf66de19456548d77b1a
Fer <fernando.falcon@holbertonstudents.com>
2025-05-06 12:29:51 -0300

    1. My first toolbox!

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print(F'{a} + {b} = {add(a, b)}')
+    print(F'{a} - {b} = {sub(a, b)}')
+    print(F'{a} * {b} = {mul(a, b)}')
+    print(F'{a} / {b} = {div(a, b)}')
