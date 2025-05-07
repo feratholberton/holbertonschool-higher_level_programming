47f5a2adc49e89ee84998500f5c5b4ac5f8bde71
Fer <fernando.falcon@holbertonstudents.com>
2025-05-07 16:03:30 -0300

    2. How to make a script dynamic! | Refactor

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,20 +2,17 @@
 import sys
 
 if __name__ == '__main__':
-    arguments = sys.argv
+    arguments = sys.argv[1:]
     arguments_qty = len(arguments)
+    counter = 1
 
-    if arguments_qty == 1:
+    if arguments_qty == 0:
         print('0 arguments.')
-    elif arguments_qty == 2:
-        counter = 1
+    elif arguments_qty == 1:
         print('1 argument:')
-        for argument in arguments[1:]:
-            print(F'{counter}: {argument}')
-            counter += 1
     else:
-        counter = 1
-        print(F'{arguments_qty - 1} arguments:')
-        for argument in arguments[1:]:
-            print(F'{counter}: {argument}')
-            counter += 1
+        print(F'{arguments_qty} arguments:')
+
+    for argument in arguments:
+        print(F'{counter}: {argument}')
+        counter += 1
