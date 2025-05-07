d0ba0cd0376f66a408e6c61e596ac02a9159af0f
Fer <fernando.falcon@holbertonstudents.com>
2025-05-07 14:52:56 -0300

    2. How to make a script dynamic!

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,15 @@ if __name__ == '__main__':
 
     if arguments_qty == 1:
         print('0 arguments.')
+    elif arguments_qty == 2:
+        counter = 1
+        print('1 arguments:')
+        for argument in arguments[1:]:
+            print(F'{counter}: {argument}')
+            counter += 1
     else:
         counter = 1
+        print(F'{arguments_qty - 1} arguments.')
         for argument in arguments[1:]:
             print(F'{counter}: {argument}')
             counter += 1
