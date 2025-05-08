d9028c40b8ff2b17506549b5734129b67a4109e3
Fer <fernando.falcon@holbertonstudents.com>
2025-05-08 14:06:07 -0300

    6. Build my own calculator!

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    # Messages
+    usage_error_massage = 'Usage: ./100-my_calculator.py <a> <operator> <b>'
+    unknown_operator_message = 'Unknown operator. Available operators: +, -, * and /'
+
+    supported_operators = ['+', '-', '*', '/']
+
+    # Get arguments excluding the file name -> sys.argv[0]
+    arguments = sys.argv[1:]
+
+    # Count retrieved arguments
+    arguments_qty = len(arguments)
+
+    def calculate():
+        # Check if arguments quantity is 3
+        if arguments_qty != 3:
+            print(usage_error_massage)
+            return 1
+        # Give name to retrieved arguments if they are three
+        else:
+            operand_a = int(arguments[0])
+            operator = arguments[1]
+            operand_b = int(arguments[2])
+
+            # Check if the operator is a supported
+            if operator not in supported_operators:
+                print(unknown_operator_message)
+                return 1
+            else:
+                if operator == '+':
+                    print(f'{operand_a} {operator} {operand_b} = {add(operand_a, operand_b)}')
+                elif operator == '-':
+                    print(f'{operand_a} {operator} {operand_b} = {sub(operand_a, operand_b)}')
+                elif operator == '*':
+                    print(f'{operand_a} {operator} {operand_b} = {mul(operand_a, operand_b)}')
+                else:
+                    print(f'{operand_a} {operator} {operand_b} = {div(operand_a, operand_b)}')
+
+    calculate()
