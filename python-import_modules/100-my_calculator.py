828280f44e4894812b1da5a8ee533213c6a45328
Fer <fernando.falcon@holbertonstudents.com>
2025-05-08 14:39:45 -0300

    Correction of "6. Build my own calculator!" | Shorter lines

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     # Messages
     usage_error_massage = 'Usage: ./100-my_calculator.py <a> <operator> <b>'
-    unknown_operator_message = 'Unknown operator. Available operators: +, -, * and /'
+    unknown_ope_msg = 'Unknown operator. Available operators: +, -, * and /'
 
     supported_operators = ['+', '-', '*', '/']
 
@@ -22,22 +22,22 @@ if __name__ == '__main__':
             exit(1)
         # Give name to retrieved arguments if they are three
         else:
-            operand_a = int(arguments[0])
+            ope_a = int(arguments[0])
             operator = arguments[1]
-            operand_b = int(arguments[2])
+            ope_b = int(arguments[2])
 
             # Check if the operator is a supported
             if operator not in supported_operators:
-                print(unknown_operator_message)
+                print(unknown_ope_msg)
                 exit(1)
             else:
                 if operator == '+':
-                    print(f'{operand_a} {operator} {operand_b} = {add(operand_a, operand_b)}')
+                    print(f'{ope_a} {operator} {ope_b} = {add(ope_a, ope_b)}')
                 elif operator == '-':
-                    print(f'{operand_a} {operator} {operand_b} = {sub(operand_a, operand_b)}')
+                    print(f'{ope_a} {operator} {ope_b} = {sub(ope_a, ope_b)}')
                 elif operator == '*':
-                    print(f'{operand_a} {operator} {operand_b} = {mul(operand_a, operand_b)}')
+                    print(f'{ope_a} {operator} {ope_b} = {mul(ope_a, ope_b)}')
                 else:
-                    print(f'{operand_a} {operator} {operand_b} = {div(operand_a, operand_b)}')
+                    print(f'{ope_a} {operator} {ope_b} = {div(ope_a, ope_b)}')
 
     calculate()
