5f8e1e35a7deb819943b1df8847eeb5b57e9e67c
Fer <fernando.falcon@holbertonstudents.com>
2025-05-21 15:17:03 -0300

    -

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -63,7 +63,6 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Compares two rectangles by area and returns the biggest one"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
