83c68a52d718574b05f14ee2688b99265a2edb2b
Fer <fernando.falcon@holbertonstudents.com>
2025-05-21 10:22:13 -0300

    Remove tabs

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -27,7 +27,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
