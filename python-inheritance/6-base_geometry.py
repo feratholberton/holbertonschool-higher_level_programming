cbbbdf288e742bc2a509954676ac8c90b7718e58
Fer <fernando.falcon@holbertonstudents.com>
2025-06-01 15:22:27 -0300

    6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''BaseGeometry - Module documentation'''
+
+
+class BaseGeometry:
+    '''Class documentation'''
+    def area(self):
+        raise Exception('area() is not implemented')
