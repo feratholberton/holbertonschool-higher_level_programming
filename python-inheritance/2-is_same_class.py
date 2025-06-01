6ecec9d6b6c09636549c9c232dbb4274a6dcfc3b
Fer <fernando.falcon@holbertonstudents.com>
2025-06-01 14:52:17 -0300

    trailing whitespace

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,8 +3,8 @@
 
 
 def is_same_class(obj, a_class):
-    '''Define a function that returns 
-    True if the object is exactly an instance of the specified class; 
+    '''Define a function that returns
+    True if the object is exactly an instance of the specified class;
     otherwise False'''
 
     return type(obj) is a_class
