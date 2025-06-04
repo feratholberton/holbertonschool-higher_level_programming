83146e27292a2c0a360f8a9f21cff3a7261fd15f
Fer <fernando.falcon@holbertonstudents.com>
2025-06-04 09:55:25 -0300

    Doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+'''Read file'''
+
 
 def read_file(filename=""):
+    '''Write a function that reads a text file (UTF8) and prints it to stdout:'''
     with open(filename, 'r', encoding="utf-8") as file:
         read_data = file.read()
         print(read_data)
