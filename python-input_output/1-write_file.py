b2e99e8f084afe32e142c1157b44e7c63279c018
Fer <fernando.falcon@holbertonstudents.com>
2025-06-04 10:59:44 -0300

    1. Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Write and return'''
+
+def write_file(filename="", text=""):
+    '''
+    Write a function that writes a string
+    to a text file (UTF8) 
+    and returns the number of characters written
+    '''
+    with open(filename, 'w', encoding='utf-8') as file:
+        written_string = file.write(text)
+        return written_string
