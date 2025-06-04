aa7d21303a198b39fba295aa2218a4ab556542dd
Fer <fernando.falcon@holbertonstudents.com>
2025-06-04 10:02:23 -0300

    no breakline

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     '''Write a function that reads a text file (UTF8) and prints it to stdout:'''
     with open(filename, 'r', encoding="utf-8") as file:
         read_data = file.read()
-        print(read_data)
+        print(read_data, end='')
