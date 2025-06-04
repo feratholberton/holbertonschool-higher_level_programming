ed9839dedd109c3111b786242a515ffdbbc1fa33
Fer <fernando.falcon@holbertonstudents.com>
2025-06-04 11:02:00 -0300

    Remove trailing whitespace

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
     '''
     Write a function that writes a string
-    to a text file (UTF8) 
+    to a text file (UTF8)
     and returns the number of characters written
     '''
     with open(filename, 'w', encoding='utf-8') as file:
