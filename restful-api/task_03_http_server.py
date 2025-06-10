a6dd88c2e8f92407976a4a5e205ff00970e64b26
Fer <fernando.falcon@holbertonstudents.com>
2025-06-10 15:47:24 -0300

    Correction of 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,58 +1,49 @@
 #!/usr/bin/python3
+"""Basic http hamdler"""
+
 import http.server
 import json
-""""""
 
-
-class SimpleRequestHandler(http.server.BaseHTTPRequestHandler):
+class CustomRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        # Home endpoint
-        if self.path == "/":
+        if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
-        # / JSON data endpoint
         elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps({
-                "name": "John", 
-                "age": 30, 
-                "city": "New York"}
-            ).encode())
+            self.wfile.write(json.dumps(
+                {"name": "John", "age": 30, "city": "New York"}).encode()
+            )
 
-        # / Status endpoint
-        elif self.path == "/status":
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"OK")
-
-        # / Info endpoint
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps({
-                "version": "1.0",
-                "description": "A simple API built with http.server"
-            }).encode())
+            self.wfile.write(json.dumps(
+                {"version": "1.0", 
+                "description": "A simple API built with http.server"}).encode()
+            )
 
-        # 404 endpoint
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"OK")
+
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"404 Not Found")
 
-def run(server_class=http.server.HTTPServer, handler_class=SimpleRequestHandler):
-    port = 8000
-    server_address = ("", port)
+def run(server_class=http.server.HTTPServer, handler_class=CustomRequestHandler):
+    server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting server on http://localhost:{port}")
     httpd.serve_forever()
 
 run()
