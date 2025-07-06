071fd45d4e0939da45c6f8bddabff4876b1918b3
Fer <fer.falcon.dg@gmail.com>
2025-07-06 18:49:20 -0300

    14. Cities in state

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""States via SQLAlchemy"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State
+from model_city import City
+
+def deleteState(user_name, user_password, database_name):
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(user_name, user_password, database_name))
+
+    session = Session(engine)
+
+    for city, state in (session.query(City, State)
+                        .filter(City.state_id == State.id)
+                        .order_by(City.id).all()):
+
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
+
+
+if __name__ == '__main__':
+    user_name = argv[1]
+    user_password = argv[2]
+    database_name = argv[3]
+
+    deleteState(user_name, user_password, database_name)
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""City model"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = 'cities'
+    id = Column(
+        Integer,
+        unique=True,
+        primary_key=True,
+        nullable=False,
+        autoincrement='auto'
+    )
+
+    name = Column(
+        String(128), 
+        nullable=False
+    )
+
+    state_id = Column(
+        Integer, 
+        ForeignKey('states.id'),
+        nullable=False
+    )
+
+    def __init__(self, name, state_id):
+        self.name = name
+        self.state_id = state_id
