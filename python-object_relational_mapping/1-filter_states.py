bbc4f4b68e602508b0e9d666f598cbad7338bbc4
Fer <fer.falcon.dg@gmail.com>
2025-07-06 13:47:23 -0300

    1. Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def states(username, userPassword, database_name):
+    """Filter states."""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=userPassword,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states " +
+        "WHERE BINARY name LIKE 'N%' " +
+        "ORDER BY states.id"
+    )
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    bd = sys.argv[3]
+
+    states(user, passw, bd)
