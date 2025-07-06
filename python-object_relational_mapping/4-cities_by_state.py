ef88aa7cf122727b9de53750d2e8e4b0480bf7f9
Fer <fer.falcon.dg@gmail.com>
2025-07-06 17:11:27 -0300

    Trailing whitespace

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,9 +18,9 @@ def states(user_name, user_password, database_name):
     cursor = database.cursor()
 
     query = (
-        "SELECT cities.id, cities.name, states.name " + 
-        "FROM cities INNER JOIN states " + 
-        "ON cities.state_id = states.id " + 
+        "SELECT cities.id, cities.name, states.name " +
+        "FROM cities INNER JOIN states " +
+        "ON cities.state_id = states.id " +
         "ORDER BY cities.id"
     )
 
