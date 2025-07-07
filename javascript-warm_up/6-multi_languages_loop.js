9a50ba468edd3f5fcff9e1b9bff8ff5cf971d0c4
Fer <fer.falcon.dg@gmail.com>
2025-07-07 15:47:18 -0300

    q

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arguments = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let arguments = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (let i = 0; i < arguments.length; i++) {
-  console.log(arguments[i]); // Accessing element by index
+  console.log(arguments[i]);
 }
 
 // arguments.forEach((item) => {
