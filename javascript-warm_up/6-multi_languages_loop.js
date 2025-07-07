c2e60994be9641087916f4df7165cedecb39743f
Fer <fer.falcon.dg@gmail.com>
2025-07-07 15:49:29 -0300

    var name

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
-let arguments = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < arguments.length; i++) {
-  console.log(arguments[i]);
-}
-
-// arguments.forEach((item) => {
-//   console.log(item);
-// });
+languages.forEach((item) => {
+  console.log(item);
+});
