25385e860516eed0ad3fe3c909b3c121f10a7fcc
Fer <fer.falcon.dg@gmail.com>
2025-07-07 14:08:16 -0300

    2. Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+import { argv } from 'node:process';
+
+const argumentsLength = argv.length
+
+if (argumentsLength === 1) {
+    console.log('No argument');
+} 
+else if (argumentsLength === 2) {
+    console.log('Argument found');
+}
+else {
+    console.log('Arguments found');
+}
