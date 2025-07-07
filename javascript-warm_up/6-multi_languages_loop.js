2385cc3a6bc604c1c320dac9897636ae4f2db5d0
Fer <fer.falcon.dg@gmail.com>
2025-07-07 15:40:52 -0300

    Semicolon

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arguments = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const arguments = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 arguments.forEach((item) => {
   console.log(`${item}`);
