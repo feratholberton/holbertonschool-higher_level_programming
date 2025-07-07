bce7a89a8ee87fd89a2178a94a253c88805a4c8c
Fer <fer.falcon.dg@gmail.com>
2025-07-07 14:24:46 -0300

    semistandard validation

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
 
-// import argv from 'node:process';
 const { argv } = require('node:process');
-
 const argumentsLength = argv.length;
 
-// console.log(argumentsLength);
-
 if (argumentsLength === 2) {
-    console.log('No argument');
-} 
-else if (argumentsLength === 3) {
-    console.log('Argument found');
-}
-else {
-    console.log('Arguments found');
+  console.log('No argument');
+} else if (argumentsLength === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
