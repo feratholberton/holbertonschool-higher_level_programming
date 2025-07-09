b7acf87c88f057f409e52b20ee76b98e6631afe8
Fer <fernando.falcon@holbertonstudents.com>
2025-07-09 14:41:02 -0300

    Correction of "7. I love C"

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,14 +2,14 @@
 
 const { argv } = require('node:process');
 
-const repeat_times = parseInt(argv[2]);
+const repeatTimes = parseInt(argv[2]);
 const phrase = 'C is fun';
 
-if (isNaN(repeat_times)) {
-    console.log('Missing number of occurrences');
+if (isNaN(repeatTimes)) {
+  console.log('Missing number of occurrences');
 } else {
   let count = 0;
-  while (count < repeat_times) {
+  while (count < repeatTimes) {
     console.log(phrase);
     count++;
   }
