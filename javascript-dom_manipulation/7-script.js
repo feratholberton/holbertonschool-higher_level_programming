a451ea7f618415f09e094d904f02a86786fcd1f3
Fer <fer.falcon.dg@gmail.com>
2025-07-14 16:12:00 -0300

    7. Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,11 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const ul = document.getElementById('list_movies');
+    data.results.forEach(film => {
+      const li = document.createElement('li');
+      li.textContent = film.title;
+      ul.appendChild(li);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
